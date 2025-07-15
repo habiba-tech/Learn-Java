c2f5700313c23e62c392fd368edac1c460689ae2
habiba Siddiqui <siddiquiummehabiba41@gmail.com>
2025-07-15 09:32:08 +0530

    Array

---
+++ Array.java
@@ -0,0 +1,10 @@
+public class Array {
+    public static void main(String[] args) {
+        int numbers[]= {12,56,45,67,3};
+        int sum = 0;
+        for(int number:numbers){
+            sum += number ;
+        }
+        System.out.println("sum is"+sum);
+    }
+}
