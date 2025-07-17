f52dbd71a70a761a5a6ce295f9a19d93ea2f6b13
habiba Siddiqui <siddiquiummehabiba41@gmail.com>
2025-07-17 09:41:17 +0530

    ColorsChanges

--- MyButton.java
+++ MyButton.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.*;
+
 public class MyButton {
     public static void main(String[] args) {
         JFrame frame = new JFrame("JButton Demo");
@@ -7,6 +9,11 @@ public class MyButton {
 
         JButton button = new JButton("Clicked Me");
         frame.add(button);
+        button.setBackground(Color.pink);
+        button.setForeground(Color.magenta);
+        button.setFont(new Font("Arial", Font.BOLD, 16));
+        button.setFocusPainted(false);
+
 
         frame.setVisible(true);
     }
